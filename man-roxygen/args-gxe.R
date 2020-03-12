2e167cc7f0a672cd1ab6563c85b65679f49519ed
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2020-03-12 14:34:41 -0400

    Improve doc for gxe

--- man-roxygen/args-gxe.R
+++ man-roxygen/args-gxe.R
@@ -1,3 +1,4 @@
 #' @param gxe a vector of data column names. Creates additional data
-#'         columns named paste0('snp_',columnName) that are the
-#'         product of the SNP and the data in columnName
+#'   columns named paste0('snp_',columnName) that are the product of
+#'   the SNP and the data in columnName. Add these new data columns to
+#'   your list of covariates to use them as covariates.
